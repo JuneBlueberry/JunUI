7593ad0b14c5ac79896e91fcd2c11abc408d0acb
JuneBlueberry <812391718@qq.com>
2018-10-18 01:16:47 +0800

    下拉选择组件完善
    下拉选择组件完善

--- src/comps/components/select/select.vue
+++ src/comps/components/select/select.vue
@@ -1,27 +1,31 @@
 <!-- JunChen 2018-10-15 下拉选择Select组件 -->
 <template>
   <div class="jun-select-div">
-    <div class="select-title" @click="selectValue">请选择</div>
+    <div class="select-title" @click="clickInputSelect">{{currentValue==''?'请选择':currentValue}}</div>
     <transition name="selectDiv">
       <div class="select-options" v-if="isSelectDivShow">
-        <!-- <ul v-for="(item, index) in data" :key="index" >
-        </ul> -->
-        <ul class="select-list"></ul>
-        <ul class="select-optgroup">
-          <li class="select-label">JavaScript</li>
-          <li class="select-option">jQuery</li>
-          <li class="select-option st_selected">ReactJS</li>
-          <li class="select-option">AngularJS</li>
-        </ul>
-        <ul class="select-optgroup">
-          <li class="select-label">HTML</li>
-          <li class="select-option" data-value="HTML5" data-index="3">HTML 5</li>
-          <li class="select-option" data-value="XML" data-index="4">XML</li>
-          <li class="select-option" data-value="XHTML" data-index="5">XHTML</li>
-        </ul>
-        <ul class="select-list">
-          <li class="select-option" data-value="css" data-index="6">CSS/SCSS/LESS</li>
-          <li class="select-option" data-value="more" data-index="7">More Options Here</li>
+        <ul v-for="(item, index) in data" 
+          :key="index" 
+          :class="item.group!=undefined?'select-optgroup':'select-list'">
+          <template v-if="item.group!=undefined">
+            <li class="select-group">{{item.group}}</li>
+            <li 
+              v-for="(itemson, index) in item.list"
+              class="select-option"
+              :key="index"
+              @click="selectValue(itemson)">
+              <span class="select-label">{{itemson.label}}</span>
+              <span v-if="itemson.remark!=undefined" class="select-remark">{{itemson.remark}}</span>
+            </li>
+          </template>
+          <template v-else>
+            <li 
+              class="select-option"
+              @click="selectValue(item)">
+              <span class="select-label">{{item.label}}</span>
+              <span v-if="item.remark!=undefined" class="select-remark">{{item.remark}}</span>
+            </li>
+          </template>
         </ul>
       </div>
     </transition>
@@ -39,7 +43,7 @@ export default {
           {
             group: 'JavaScript',
             list: [
-              {label: 'jQuery', value: 'jQuery'},
+              {label: 'jQuery', value: 'jQuery', remark: '一个流行的JS库'},
               {label: 'ReactJS', value: 'ReactJS'},
               {label: 'AngularJS', value: 'AngularJS'}
             ]
@@ -60,6 +64,7 @@ export default {
   },
   data() {
     return {
+      currentValue: '',
       isSelectDivShow: false
     };
   },
@@ -69,8 +74,20 @@ export default {
   computed: {},
 
   methods: {
-    selectValue: function(){
+    clickInputSelect: function(){
       this.isSelectDivShow = !this.isSelectDivShow
+    },
+
+    selectValue: function(item){
+      this.currentValue = item.label
+      this.isSelectDivShow = false
+      this.$emit('on-select', item)
+    }
+  },
+
+  watch: {
+    currentValue: function(value){
+      this.$emit('change', value)
     }
   }
 };
