578dfe6537ffc8879d62dcfd5604f58ef9512a67
JuneBlueberry <812391718@qq.com>
2018-11-05 23:43:47 +0800

    Table组件新建
    Table组件新建

---
+++ src/comps/components/table/table.vue
@@ -0,0 +1,23 @@
+<!-- JunChen 2018-11-05 Table组件 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {},
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+</script>
